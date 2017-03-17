4fb9bd1fabc933dc572ed1c7d0d873f6cbef2663
Manveer Bhangu <manu.bhangu5530@gmail.com>
2017-03-17 14:33:10 -0700

    Better understanding of console, html canvas, and video elements
    Signed-off-by: Manveer Bhangu <manu.bhangu5530@gmail.com>

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,74 @@
+// Elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const playButton = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+//console.dir(video);
+
+
+//Functions
+function togglePlay()
+{
+	if(video.paused)
+	{
+		video.play();
+	} else {
+		video.pause();
+	}
+}
+
+function updateButton()
+{
+	const icon = this.paused ? '►' : '❚ ❚';
+	playButton.textContent = icon;
+	console.log('button updated');
+}
+
+function skip()
+{
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate()
+{
+	video[this.name] = this.value;
+}
+
+function handleProgress()
+{
+	const percentComplete = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percentComplete}%`;
+}
+
+function scrub(event)
+{
+	const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+}
+
+//Event Listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+
+playButton.addEventListener('click', togglePlay);
+skipButtons.forEach(skipbutton => skipbutton.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (event) => {
+	if(mousedown)
+	{
+		scrub(event);
+	}
+});
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
